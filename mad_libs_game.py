6b6c80ed8f74f421468e0671aa80eea8506df940
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2022-07-05 18:49:43 +0300

    Create mad_libs_game.py

---
+++ mad_libs_game.py
@@ -0,0 +1,9 @@
+adjective = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_tech_person = input("Famous tech person: ")
+
+madlib = f"Searching for a job is so {adjective}! \
+I like to {verb1}. Stay positive and {verb2} like you are {famous_tech_person}!"
+
+print(madlib)
